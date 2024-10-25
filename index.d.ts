484c3d07bae6f118ebabd6fc6662e3d429f02505
CaptainGoatee <sellsj69@gmail.com>
2024-10-25 19:46:48 +0100

    Added new Handler features 0.2.2

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,9 @@
+/** @format */
+
 declare module "goated-discord-handler" {
   import { Client, APIApplicationCommand } from "discord.js";
   import { Logger } from "winston";
 
-  
   interface DiscordHandlerOptions {
     client: Client; // The Discord client instance
     token: string; // The bot token for authentication
@@ -10,7 +11,9 @@ declare module "goated-discord-handler" {
     eventsPath?: string; // Optional path for event files
     validationsPath?: string; // Optional path for validation files
     testServer?: string; // Optional test server ID
-    logger?: Logger; // Optional logger instance for logging
+    logger?: Logger; // Optional logger instance for logging,
+    refreshCommands?: boolean; // Optional Reloader via discord
+    toggleCommands?: boolean; // Optional toggle commands via discord
   }
 
   interface LocalCommand extends APIApplicationCommand {
